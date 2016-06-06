1fd16c23cb1b242915dc5c4ad844d99f0ee48079
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-06-07 03:46:57 +0800

    Update

--- ms/docs/_assets/script.js
+++ ms/docs/_assets/script.js
@@ -5,11 +5,22 @@ window.jQuery = $
 var Waypoint = require('waypoints/lib/jquery.waypoints')
 
 $(function () {
+  var $pages = $('.page-section')
+  var count = $pages.length
+  $('[role~="page-count"]').html(count)
+})
+
+$(function () {
+  var $pages = $('.page-section')
+  var $number = $('[role~="page-number"]')
+  var count = $pages.length
+
   $('.page-section').addClass('-mute')
   $('.page-section').waypoint({
     handler: function (direction) {
       $('.page-section.-active').addClass('-mute').removeClass('-active')
       $(this.element).removeClass('-mute').addClass('-active')
+      $number.html($pages.index(this.element) + 1)
     },
     offset: '50%'
   })
