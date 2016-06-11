5a4d791642c64057312546a0e91d41d1ab2f2c85
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-06-12 01:28:04 +0800

    Compress things, use Zepto

--- metalsmith.js
+++ metalsmith.js
@@ -1,17 +1,32 @@
 var Metalsmith = require('metalsmith')
 
+var isProduction = process.env.NODE_ENV === 'production'
 var app = Metalsmith(__dirname)
   // Add extra meta info
   .use(require('./lib/meta')())
 
   // Browserify
-  .use(require('metalsmith-browserify')({
+  .use(tap(require('metalsmith-browserify')({
+    dest: 'assets/common.js'
+  }), b => {
+    b.bundle.require('jquery')
+  }))
+  .use(tap(require('metalsmith-browserify')({
     dest: 'assets/script.js',
     args: [ './docs/assets/script.js' ]
+  }), b => {
+    b.bundle.external('jquery')
   }))
-  .use(require('metalsmith-browserify')({
+  .use(tap(require('metalsmith-browserify')({
     dest: 'assets/example.js',
     args: [ './lib/web-example/index.js' ]
+  }), b => {
+    b.bundle.external('jquery')
+  }))
+  .use(tap(require('metalsmith-browserify')({
+    dest: 'vendor/redux.js'
+  }), b => {
+    b.bundle.require('redux')
   }))
 
   // Sass
@@ -23,6 +38,10 @@ var app = Metalsmith(__dirname)
   }))
   .use(require('./lib/middleware').transformHtml())
   .use(require('./lib/example').embedExamples())
+  .use(require('metalsmith-uglify')({
+    sourceMap: !isProduction,
+    compress: isProduction
+  }))
   .source('./docs')
   .destination('./public')
 
@@ -30,4 +49,9 @@ if (module.parent) {
   module.exports = app
 } else {
   app.build(function (err) { if (err) throw err })
+}
+
+function tap (object, fn) {
+  fn(object)
+  return object
 }
